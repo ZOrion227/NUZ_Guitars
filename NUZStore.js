23268081a299e68b6a970a91e7ddb0ca03d66819
Nathan Renfro <nathan.renfro490@524-11a5g8wn.local>
2017-05-12 16:41:58 -0700

    Version 0.0; blank slate with a proper file hierarchy, 3 HTML files each with a Bootstrap template base, as well as accompanying CSS and JS files.

---
+++ NUZStore.js
@@ -0,0 +1,9 @@
+/*
+   NUZ Guitars - A mock small business
+
+   Authors:          Nate Renfro, Ulyses Parra, Zerrick Orion
+   Date:             5.12.17
+
+   Filename:         NUZStore.js
+   Supporting files: none
+*/
